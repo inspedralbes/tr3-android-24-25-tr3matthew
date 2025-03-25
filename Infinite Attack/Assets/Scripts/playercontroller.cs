a6d634705a2840da754429f45ef69467ff2d2fe5
a23matcasmar <a23matcasmar@inspedralbes.cat>
2025-03-25 13:27:54 +0100

    He corregido los movimientos

--- Infinite Attack/Assets/Scripts/playercontroller.cs
+++ Infinite Attack/Assets/Scripts/playercontroller.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
-    public float speed;
-    public float fuerzaSalto;
-
+    public float speed = 10f;
+    public float fuerzaSalto = 20f;
+    public Collider2D cantanteCollider;
+    public float saltosMaximos;
     private Rigidbody2D rigidBody;
-    private 
+    private bool grounded = true;
+    private float saltosRestantes;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,9 +30,28 @@ public class playercontroller : MonoBehaviour
 
     void Salto()
     {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && saltosRestantes > 0)
         {
             rigidBody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            saltosRestantes--;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Bordes"))
+        {
+            saltosRestantes = saltosMaximos;
+            grounded = true;
+            Debug.Log(grounded);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collison)
+    {
+        if(collison.gameObject.CompareTag("Bordes"))
+        {
+            grounded = false;
+            Debug.Log(grounded);
         }
     }
 }
